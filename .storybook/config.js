fbb547fc8c8177522c4ba66bd0af33fc48345c5b
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-07-24 14:42:56 +0800

    init

---
+++ .storybook/config.js
@@ -0,0 +1,21 @@
+// import { configure } from '@storybook/vue';
+
+// // automatically import all files ending in *.stories.js
+// const req = require.context('../stories', true, /\.stories\.js$/);
+// function loadStories() {
+//   req.keys().forEach(filename => req(filename));
+// }
+
+// configure(loadStories, module);
+
+import { configure } from '@storybook/vue';
+// automatically import all files ending in *.stories.js
+const req = require.context(
+  '../packages',
+  true,
+  /\.stories\.js$/
+);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+configure(loadStories, module);
