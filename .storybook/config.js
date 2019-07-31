3c133973ee5566b02611cb872de6be8782272e7d
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-07-31 18:14:01 +0800

    build: change workflow

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,21 +1,7 @@
-// import { configure } from '@storybook/vue';
-
-// // automatically import all files ending in *.stories.js
-// const req = require.context('../stories', true, /\.stories\.js$/);
-// function loadStories() {
-//   req.keys().forEach(filename => req(filename));
-// }
-
-// configure(loadStories, module);
-
-import { configure } from '@storybook/vue';
+import { configure } from '@storybook/vue'
 // automatically import all files ending in *.stories.js
-const req = require.context(
-  '../packages',
-  true,
-  /\.stories\.js$/
-);
+const req = require.context('../packages', true, /\.stories\.js$/)
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach(filename => req(filename))
 }
-configure(loadStories, module);
+configure(loadStories, module)
--- boilerplate/boilerplate.stories.js
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/vue'
-import Boilerplate from './src/App.vue'
-
-storiesOf('Boilerplate', module).add('normal', () => ({
-  components: { Boilerplate },
-  template: `
-      <Boilerplate />
-    `
-}))
